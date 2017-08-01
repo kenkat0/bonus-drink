ab28df3c2241a785f063405456619333d1571007
KatoKen <ken@Ken.local>
2017-08-02 00:53:40 +0900

    answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,12 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    empty_bins = amount
+    bonus_bins = 0
+    while empty_bins >= 3
+      bonus_bins = empty_bins / 3
+      amount += bonus_bins
+      empty_bins = bonus_bins + empty_bins % 3
+    end
+    amount
   end
 end
